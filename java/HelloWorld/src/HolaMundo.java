f3887330badbde6c8cc1b4d18168e87b867b724c
Ivan Mamani <ivan.mamani.q@ucb.edu.bo>
2022-10-02 20:13:02 -0400

    Tercera subida

--- java/HelloWorld/src/HolaMundo.java
+++ java/HelloWorld/src/HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Ivan Sergio Mamani Quispe, Holaaaa mundo");
+        System.out.println("Prueba con intellij");
+        System.out.println("Prueba con intellij 22");
     }
 }
