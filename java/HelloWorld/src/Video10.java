42b9cac02c2daa4419cd179d4d90a9ce8433f32a
Ivan Mamani <ivan.mamani.q@ucb.edu.bo>
2022-10-02 21:52:51 -0400

    Videos 9 y video 10

---
+++ java/HelloWorld/src/Video10.java
@@ -0,0 +1,18 @@
+public class Video10 {
+    public static void main(String[] args) {
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toLowerCase() = " + saludar.toLowerCase());
+        int numero = 10;
+        System.out.println("numero = " + numero);
+        boolean valor=true;
+        int numero2=5;
+        if (valor){
+            numero2=10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3= "15";
+        System.out.println("numero3 = " + numero3);
+    }
+}
