db5fc56ac5421287533fcb819166390bd0545c0d
Aivan-17 <ivan.mamani.q@ucb.edu.bo>
2023-06-11 00:05:51 -0400

    pruebaaaaa

---
+++ java/HelloWorld/src/ivan.java
@@ -0,0 +1,7 @@
+public class ivan {
+    public static void main(String[] args) {
+        System.out.println("Ivan Sergio Mamani Quispe, Holaaaa mundo");
+        System.out.println("Prueba con intellij");
+        System.out.println("Prueba con intellij 22");
+    }
+}
